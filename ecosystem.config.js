f5230978794c6c87f0231840370cf90ff501bc68
Philipp Kursawe <phil.kursawe@gmail.com>
2019-11-25 13:27:09 +0100

    fix: Remove key from ecosystem pm2 file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,6 @@ module.exports = {
       user: "pke",
       host: "cronos.illunis.net",
       port: "36077",
-      key: "~/.ssh/id_rsa.pub",
       ref: "origin/master",
       repo: "git@deployment:pke/xstate.io.git",
       path: "/var/www/restlr.io",
