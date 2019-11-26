d1ce4e645c5425496f3eaaf486b357a02df12ec8
Philipp Kursawe <phil.kursawe@gmail.com>
2019-11-26 12:01:39 +0100

    feat: Add SSE support
    todo: Add endpoint to SIREN response as link with rel = "monitor"

--- src/koaApp.js
+++ src/koaApp.js
@@ -5,6 +5,7 @@ const Router = require("koa-trie-router")
 
 const createStateApp = require("./xstate/xstateApp")
 const appUrl = require("./appUrl")
+const toSiren = require("./toSiren")
 
 const app = new Koa()
 const router = new Router()
@@ -13,30 +14,72 @@ app.context.resourceUrl = function(id, path = "") {
   return appUrl(this.params.app, this.href, id, path)
 }
 
+const { streamEvents } = require("http-event-stream")
+
+function sampleEvents(ctx) {
+  const fetchEventsSince = async (lastEventId) => {
+    return [ /* all events since event with ID `lastEventId` woud go here */ ]
+  }
+  return streamEvents(ctx.req, ctx.res, {
+    async fetch (lastEventId) {
+      // This method is mandatory to replay missed events after a re-connect
+      return fetchEventsSince(lastEventId)
+    },
+    stream (stream) {
+      const listener = state => {
+        if (state.changed) {
+          stream.sendEvent({
+            event: "change",
+            data: JSON.stringify(toSiren(ctx.state.service, {
+              href(path = "") {
+                return ctx.resourceUrl(ctx.state.id, path)
+              }
+            }))
+          })
+        }
+      }
+      ctx.state.service.onTransition(listener)
+      return () => ctx.state.service.off(listener)
+    }
+  })
+}
+
+const services = {}
+
 const stateAppRoutes = new Router()
   .use(async (ctx, next) => {
     const { app, id } = ctx.params
-    let machine
-    if (/^[a-f0-9]{32}$/i.test(app)) {
-      const fromGist = require("./fromGist")
-      machine = await fromGist(app)
-    } else {
-      try {
-        machine = require(`./xstate/${app}`)
-      } catch (err) {
-        ctx.throw(404, `App ${app} not found`, { app })
-      }
-    }
-
-    ctx.state.model = createStateApp(() => machine)
-
     if (id) {
-      const found = ctx.state.model.find(id)
+      const found = services[id]
       if (!found) {
         ctx.throw(404, `${app} with id ${id} not found`, { app, id })
       }
-      ctx.state.service = found.service
-      ctx.state.id = found.id
+      ctx.state.model = createStateApp(() => found.machine)
+      ctx.state.service = found
+      ctx.state.id = id
+    } else {
+      let machine
+      if (/^[a-f0-9]{32}$/i.test(app)) {
+        const fromGist = require("./fromGist")
+        machine = await fromGist(app)
+      } else {
+        try {
+          machine = require(`./xstate/${app}`)
+        } catch (err) {
+          ctx.throw(404, `App ${app} not found`, { app })
+        }
+      }
+
+      ctx.state.model = createStateApp(() => machine)
+
+      if (id) {
+        const found = ctx.state.model.find(id)
+        if (!found) {
+          ctx.throw(404, `${app} with id ${id} not found`, { app, id })
+        }
+        ctx.state.service = found.service
+        ctx.state.id = found.id
+      }
     }
     await next()
     if (ctx.state.id) {
@@ -58,10 +101,16 @@ const stateAppRoutes = new Router()
       }]
     }
   })
+  .get("/:app/:id/events", ctx => {
+    sampleEvents(ctx)
+    // Koa quirk: Don't close the request/stream after handling the route!
+    ctx.respond = false
+  })
   .post("/:app", ctx => {
     const created = ctx.state.model.create(ctx.query)
     ctx.state.id = created.id
     ctx.state.service = created.service
+    services[created.id] = created.service
     ctx.status = 201
   })
   .put("/:app/:id/:type", async ctx => {
@@ -74,7 +123,7 @@ const stateAppRoutes = new Router()
       ctx.status = 406
       return
     }
-    const changed = await ctx.state.model.send({ id: ctx.state.id, service: ctx.state.service}, event)
+    await ctx.state.model.send({ id: ctx.state.id, service: ctx.state.service}, event)
     ctx.status = 200
   })
   .get("/:app/:id", ctx => {
