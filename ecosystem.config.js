c7247bff527f5e7181bc1189d2c4aadb2b023367
Philipp Kursawe <phil.kursawe@gmail.com>
2019-11-28 13:37:13 +0100

    fix: Disable strict host checking
    No clue how that works on GH workflows :(

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,12 +11,12 @@ module.exports = {
     production: {
       user: "pke",
       host: "cronos.illunis.net",
-      ssh_options: "StrictHostKeyChecking=no",
       port: "36077",
+      ssh_options: "StrictHostKeyChecking=no",
       ref: "origin/master",
       repo: "git@deployment:pke/xstate.io.git",
       path: "/var/www/restlr.io",
-      "post-deploy": "npm ci --only=production && npm run restart"
+      "post-deploy": "npm ci && pm2 startOrRestart ecosystem.config.js"
     }
   }
 }
